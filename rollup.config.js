be1282079e12b29b80fa80023762daf7d4dfda3b
Alex Prokop <alex.prokop@close.io>
2020-12-15 12:00:50 +0000

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    commonjs(),
+  ],
+};
---
+++ src/index.js
@@ -0,0 +1,69 @@
+import { getQueriesForElement, prettyDOM } from '@testing-library/dom';
+
+if (typeof afterEach === 'function' && !process.env.RTL_SKIP_AUTO_CLEANUP) {
+  afterEach(() => {
+    cleanup();
+  });
+}
+
+const mountedViews = new Set();
+
+function render(
+  View,
+  {
+    el,
+    container,
+    baseElement = container,
+    autoRender = true,
+    ...options
+  } = {},
+) {
+  // Match react-testing-library's baseElement/container approach exactly:
+  if (!baseElement) {
+    baseElement = document.body;
+  }
+  if (!container) {
+    container = baseElement.appendChild(document.createElement('div'));
+  }
+  // if no el is supplied BB will create one in _ensureElement
+  const view = new View({ el, ...options });
+  container.appendChild(view.el);
+
+  if (autoRender) {
+    // sometimes you might not want to render immediately, eg. wrap render in act
+    view.render();
+  }
+
+  mountedViews.add({
+    view,
+    container,
+  });
+
+  return {
+    view,
+    container,
+    baseElement,
+    debug: (debugEl = baseElement, maxLength, options) =>
+      console.log(prettyDOM(debugEl, maxLength, options)),
+    unmount: () => cleanupView(view),
+    ...getQueriesForElement(baseElement),
+  };
+}
+
+function cleanup() {
+  mountedViews.forEach(cleanupView);
+}
+
+function cleanupView(viewData) {
+  const { view, container } = viewData;
+  view.remove();
+
+  if (container.parentNode === document.body) {
+    document.body.removeChild(container);
+  }
+
+  mountedViews.delete(viewData);
+}
+
+export * from '@testing-library/dom';
+export { render, cleanup };
